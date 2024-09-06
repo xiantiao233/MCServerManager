437464d5fb32c783a3d05b1c8b0b84a5fbcbc4e0
xiantiao <3054587546@qq.com>
2024-09-06 23:58:00 +0800

    feat: log4j2

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,12 @@ dependencies {
     compileOnly("org.jetbrains:annotations:16.0.1")
     // https://mvnrepository.com/artifact/com.google.code.gson/gson
     implementation("com.google.code.gson:gson:2.8.9")
+
+    // Log4j 2.x 依赖
+    implementation("org.apache.logging.log4j:log4j-api:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks.test {
