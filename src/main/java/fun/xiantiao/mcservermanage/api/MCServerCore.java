4ceb82fe7d19ce1849f5601128a4d6f81bf7c6a3
xiantiao <3054587546@qq.com>
2024-09-07 07:34:19 +0800

    feat: MCServerCore

---
+++ src/main/java/fun/xiantiao/mcservermanage/api/MCServerCore.java
@@ -0,0 +1,7 @@
+package fun.xiantiao.mcservermanage.api;
+
+public interface MCServerCore extends Jar {
+    String getCoreName();
+    String getGameVersion();
+    String getBuildVersion();
+}
---
+++ src/main/java/fun/xiantiao/mcservermanage/impl/MCServerCoreImpl.java
@@ -0,0 +1,48 @@
+package fun.xiantiao.mcservermanage.impl;
+
+import fun.xiantiao.mcservermanage.api.MCServerCore;
+import fun.xiantiao.mcservermanage.utils.FileX;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+public class MCServerCoreImpl extends JarImpl implements MCServerCore {
+
+    private final FileX file;
+
+    public MCServerCoreImpl(@NotNull FileX file) {
+        super(file.getAbsolutePath());
+        this.file = file;
+    }
+
+
+    public MCServerCoreImpl(@NotNull String pathname) {
+        super(pathname);
+        file = new FileX(pathname);
+    }
+
+    @Override
+    public String getCoreName() {
+        return split(1);
+    }
+
+    @Override
+    public String getGameVersion() {
+        return split(2);
+
+    }
+
+    @Override
+    public String getBuildVersion() {
+        return split(3);
+
+    }
+
+    private String split(int i) {
+        String name = file.getName().replaceAll(".jar","");
+        String[] split = name.split("-");
+        if (split.length != 4 || !"core".equals(split[0])) throw new IllegalCallerException("jar 文件错误["+file.getParent()+"]");
+        else return split[i];
+    }
+
+}
