682733c18985cf9dc9d36c9811972cf6b29b01ec
xiantiao <3054587546@qq.com>
2024-09-07 08:29:47 +0800

    feat<FileX>: moveFile, copyFile

--- src/main/java/fun/xiantiao/mcservermanage/utils/FileX.java
+++ src/main/java/fun/xiantiao/mcservermanage/utils/FileX.java
@@ -3,6 +3,12 @@ package fun.xiantiao.mcservermanage.utils;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+import static fun.xiantiao.mcservermanage.Main.getLogger;
 
 public class FileX extends File {
     public FileX(@NotNull String pathname) {
@@ -20,5 +26,54 @@ public class FileX extends File {
             fileXs[i] = new FileX(files[i]);
         }
         return fileXs;
+    }
+
+    public boolean renameTo(@NotNull String newPath) {
+        File newFile = new File(newPath);
+
+        // Check if the new path is a directory
+        if (newFile.isDirectory()) {
+            getLogger().debug("Error: The new path is a directory, not a file.[{}]", newPath);
+            return false;
+        }
+
+        // Attempt to rename
+        boolean success = this.renameTo(newFile);
+        if (!success) {
+            getLogger().debug("Rename failed: {} -> {}", this.getAbsolutePath(), newPath);
+        }
+        return success;
+    }
+
+    public boolean moveFile(File target) {
+        return moveFile(target.getAbsolutePath());
+    }
+    public boolean moveFile(String target) {
+        Path targetPath = new File(target+"/"+this.getName()).toPath();
+        try {
+            Files.move(this.toPath(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+            getLogger().debug("File moved successfully. {} -> {}", this.getAbsolutePath(), targetPath);
+            return true;
+        } catch (IOException e) {
+            getLogger().error(e);
+            getLogger().debug("Failed to move file. {} -> {}", this.getAbsolutePath(), targetPath);
+            return false;
+        }
+    }
+
+    public boolean copyFile(File file) {
+        return copyFile(file.getAbsolutePath());
+    }
+    public boolean copyFile(String target) {
+        Path targetPath = new File(target+"/"+this.getName()).toPath();
+        try {
+            Files.copy(this.toPath(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+            getLogger().debug("File copied successfully. {} -> {}", this.getAbsolutePath(), targetPath);
+            return true;
+        } catch (IOException e) {
+            getLogger().error(e);
+            getLogger().debug("Failed to copy file. {} -> {}", this.getAbsolutePath(), targetPath);
+            return false;
+        }
     }
 }
