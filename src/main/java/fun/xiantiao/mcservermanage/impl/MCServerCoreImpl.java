d7dc334e6c7e247c180b5c9dd6107f07601e0d40
xiantiao <3054587546@qq.com>
2024-09-07 09:06:32 +0800

    fix<MCServerCoreImpl>: out error

--- src/main/java/fun/xiantiao/mcservermanage/impl/MCServerCoreImpl.java
+++ src/main/java/fun/xiantiao/mcservermanage/impl/MCServerCoreImpl.java
@@ -41,7 +41,7 @@ public class MCServerCoreImpl extends JarImpl implements MCServerCore {
     private String split(int i) {
         String name = file.getName().replaceAll(".jar","");
         String[] split = name.split("-");
-        if (split.length != 4 || !"core".equals(split[0])) throw new IllegalCallerException("jar 文件错误["+file.getParent()+"]");
+        if (split.length != 4 || !"core".equals(split[0])) throw new IllegalCallerException("jar 文件错误["+file+"]");
         else return split[i];
     }
 
